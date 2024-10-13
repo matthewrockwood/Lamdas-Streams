b8cfa18dc00fa0d71373c9e4224769050fb2f066
matthewrockwood <rockwoodmatthew@gmail.com>
2024-10-13 00:57:35 -0400

    ad

---
+++ src/main/java/org/example/ArraysAndStreams2.java
@@ -0,0 +1,80 @@
+package org.example;
+
+// Fig. 17.12: ArraysAndStreams2.java
+// Demonstrating lambdas and streams with an array of Strings.
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ArraysAndStreams2 {
+    public static void main(String[] args) {
+        String[] strings =
+                {"Red", "orange", "Yellow", "green", "Blue", "indigo", "Violet"};
+
+
+
+        // display original strings
+        System.out.printf("Original strings: %s%n", Arrays.asList(strings));
+
+        // strings in uppercase
+        System.out.printf("strings in uppercase: %s%n",
+                Arrays.stream(strings)
+                        .map(String::toUpperCase)
+                        .collect(Collectors.toList()));
+
+        // strings less than "n" (case insensitive) sorted ascending
+        System.out.printf("strings less than n sorted ascending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n") < 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER)
+                        .collect(Collectors.toList()));
+
+        // strings less than "n" (case insensitive) sorted descending
+        System.out.printf("strings less than n sorted descending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n") < 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                        .collect(Collectors.toList()));
+
+        //to lowercase if a vowel
+        System.out.printf("strings that start with a vowel: %s%n",
+                Arrays.stream(strings)
+                        .map(x -> x.toLowerCase())
+                        .filter(x-> x.startsWith("a") || x.startsWith("e") ||
+                                x.startsWith("i") || x.startsWith("o") ||
+                                x.startsWith("u"))
+                        .toList());
+
+        //idk
+        System.out.printf("strings concatenated: %s%n",
+                Arrays.stream(strings)
+                        .collect(Collectors.joining(", ")));
+
+        //part 3
+        System.out.printf("Count of strings that contain more than 5 %d%n",
+                Arrays.stream(strings)
+                        .filter(x -> x.length()> 5)
+                        .collect(Collectors.toList())
+                        .size()
+//found out u can also use .count but whatever
+                );
+
+    }
+
+}
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
